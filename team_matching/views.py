c1deaa0482bd6e4433755f519238905e55497dca
hiyori99 <luluikdkoko@gmail.com>
2021-09-19 19:32:22 +0900

    first commit

---
+++ team_matching/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+# def index(request):
+#   return HttpResponse("Hello")
+
+def index(request):
+  return render(request, "hello/index.html")
+
+def brian(request):
+  return  HttpResponse("Hello, Brian")
+
+def greet(request, name):
+  return HttpResponse(f"Hello, {name.capitalize()}!") #名前の先頭を大文字にする
